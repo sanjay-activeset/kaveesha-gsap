b6fe1f6cfe87513089712e31c41b24b473bec5fb
Rehan Khurshid <rehankhurshid1@gmail.com>
2025-03-16 17:13:52 +0530

    test

--- src/home/section-hero.js
+++ src/home/section-hero.js
@@ -1,4 +1,6 @@
 import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
 
 export const sectionGallery = () => {
   // Disable scrolling function
