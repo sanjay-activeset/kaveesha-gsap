3d9fb8d779f4668a3bf9e00870d57650a28dda29
Rehan Khurshid <rehankhurshid1@gmail.com>
2025-03-16 17:11:16 +0530

    test

--- src/home/index.js
+++ src/home/index.js
@@ -3,7 +3,12 @@
  * For Home page js
  * Add or delete these files as per needed.
  */
+
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
 import { sectionGallery } from "./section-hero";
+
 console.log("Hello from Home folder");
 
 /* Run the hero animation after DOM Loaded Content */
